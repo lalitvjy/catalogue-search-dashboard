2072b89d0ddeab82375735beab605873b4b556d8
Ismail <ismail.biswas@styledotme.com>
2025-09-26 11:52:57 +0530

    works with input and copy image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import 'react-image-crop/dist/ReactCrop.css'
 import Providers from '@/components/Providers'
 import Footer from '@/components/Footer'
 import FeedbackButton from '@/components/FeedbackButton'
