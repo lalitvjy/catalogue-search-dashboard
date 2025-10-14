5d70055c4531f9782924a38e2def42467f78eb00
Ismail <ismail.biswas@styledotme.com>
2025-10-14 15:37:44 +0530

    use production key

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,14 +9,14 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     // Initialize PostHog once on the client
     if (typeof window !== 'undefined') {
-      posthog.init('phc_jMPiP1J7GdhVMTZdWB2B8Ob6u2n610C2MMIggSpKbw5', {
+      posthog.init('phc_6EQCZnhlSNEbwi91C21L9qqYpPjorWO1Drj2hUkyIO1', {
         api_host: 'https://us.i.posthog.com',
         person_profiles: 'always',
         // PostHog automatically tracks SPA pageviews; no manual capture needed
         // Test API key
         //phc_jMPiP1J7GdhVMTZdWB2B8Ob6u2n610C2MMIggSpKbw5
         // Production API key
-        //phc_jMPiP1J7GdhVMTZdWB2B8Ob6u2n610C2MMIggSpKbw5
+        //phc_6EQCZnhlSNEbwi91C21L9qqYpPjorWO1Drj2hUkyIO1 
       })
     }
   }, [])
